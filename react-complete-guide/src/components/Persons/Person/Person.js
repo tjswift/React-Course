73e08e755d03da504fe3a2bdb77d7ce67cfb77d9
Tristen Swift <tristenswift@gmail.com>
2019-07-17 20:10:01 -0700

    Updated 071719

--- react-complete-guide/src/components/Persons/Person/Person.js
+++ react-complete-guide/src/components/Persons/Person/Person.js
@@ -1,16 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+
+import Auxilary from '../../../hoc (higher order component)/Auxilary'
 import classes from "./Person.module.css";
 
 class Person extends Component {
     render(){
        console.log('[Person.js] rendering...')
-        return (
-            <div className={classes.Person}>
-                <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
-                <p>{this.props.children}</p>
-                <input type="text" onChange={this.props.changed} value={this.props.name}></input>
-            </div>
-        ) 
+        return(
+        <Fragment>
+            <p onClick={this.props.click}>
+                I'm {this.props.name} and I am {this.props.age} years old!
+            </p>,
+            <p>{this.props.children}</p>,
+            <input 
+                type="text" 
+                onChange={this.props.changed} 
+                value={this.props.name} />
+        </Fragment>
+        ); 
     }
     
 }
