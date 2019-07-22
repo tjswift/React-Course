fde87ccb37270b9eca56a729e8a78a891f536f2e
tjswift <49222993+tjswift@users.noreply.github.com>
2019-07-22 16:53:20 -0700

    Added loops to Burger.js

--- burger-builder/src/components/Burger/Burger.js
+++ burger-builder/src/components/Burger/Burger.js
@@ -1,13 +1,25 @@
 import React from 'react'
 
 import classes from './Burger.module.css';
-import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+
 const burger = (props) => {
-    return(
+    const transformedIngredients = Object.keys(props.ingredients)
+    console.log(transformedIngredients)
+    let ingredients = [];
+
+    for (let i = 0; i < transformedIngredients.length; i++) {
+        for (let j = 0; j < props.ingredients[transformedIngredients[i]]; j++) {
+            console.log(transformedIngredients[i])
+            ingredients.push(<BurgerIngredient type={transformedIngredients[i]} />)
+        }
+    }
+    console.log(ingredients)
+
+    return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
+            {ingredients}
             <BurgerIngredient type="bread-bottom" />
         </div>
     );
--- burger-builder/src/components/Layout/Layout.js
+++ burger-builder/src/components/Layout/Layout.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Aux from '../../hoc/Aux'
+import Aux from '../../hoc/Auxilary'
 import classes from './Layout.module.css'
 
 const layout = (props) => (
