556f21384f9ecafb4feda7952a69030d70f6ceee
Tristen Swift <tristenswift@gmail.com>
2019-07-22 20:44:07 -0700

    added some dynamic content

--- burger-builder/src/components/Burger/Burger.js
+++ burger-builder/src/components/Burger/Burger.js
@@ -4,25 +4,26 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients)
-    console.log(transformedIngredients)
-    let ingredients = [];
 
-    for (let i = 0; i < transformedIngredients.length; i++) {
-        for (let j = 0; j < props.ingredients[transformedIngredients[i]]; j++) {
-            console.log(transformedIngredients[i])
-            ingredients.push(<BurgerIngredient type={transformedIngredients[i]} />)
+        const transformedIngredients = Object.keys(props.ingredients)
+        let ingredients = [];
+
+        for (let i = 0; i < transformedIngredients.length; i++) {
+            for (let j = 0; j < props.ingredients[transformedIngredients[i]]; j++) {
+                console.log(transformedIngredients[i])
+                ingredients.push(<BurgerIngredient key={transformedIngredients[i] + j} type={transformedIngredients[i]} />)
+            }   
         }
-    }
-    console.log(ingredients)
-
+        
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            {ingredients}
+                {ingredients.length > 0 ? ingredients : <p>Please add some ingredients!</p>}
             <BurgerIngredient type="bread-bottom" />
         </div>
     );
-}
+};
+        
+    
 
 export default burger
