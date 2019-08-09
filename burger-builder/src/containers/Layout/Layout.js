66fc9462e4fdc9b46830cbfdc0e77d8a1a8f7169
tjswift <49222993+tjswift@users.noreply.github.com>
2019-08-09 11:44:13 -0700

    commit

---
+++ burger-builder/src/containers/Layout/Layout.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+
+import Aux from '../Auxilary'
+import classes from './Layout.module.css'
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar'
+import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer'
+
+
+//general layout component
+class Layout extends Component {
+    state = {
+        showSideDrawer: false
+    }
+
+    sideDrawerClosedHandler = () => {
+        this.setState({ showSideDrawer: false })
+    }
+
+    sideDrawerToggleHandler = () => {
+        this.setState((prevState) => {
+            return { showSideDrawer: !prevState.showSideDrawer }
+        });
+    }
+
+
+    render() {
+        return (
+            <Aux>
+                <Toolbar
+                    drawerToggleClicked={this.sideDrawerToggleHandler} />
+                <SideDrawer
+                    closed={this.sideDrawerClosedHandler}
+                    open={this.state.showSideDrawer} />
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        )
+    }
+}
+
+export default Layout
